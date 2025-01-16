af29198bc9321f033d7ec58837fbf9d8073a1b25
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-01-16 19:08:55 +0600

    added create post API

--- index.js
+++ index.js
@@ -1,17 +1,78 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8nuar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    const ChatterPoint = client.db("ChatterPoint");
+    const userCollection = ChatterPoint.collection("users");
+    const postCollection = ChatterPoint.collection("posts");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    app.post("/users/register", async (req, res) => {
+      const newUser = req.body;
+
+      try {
+        const existingUser = await userCollection.findOne({ email: newUser.email });
+        if (existingUser) {
+          return res.status(400).json({ message: "Email already exists" });
+        }
+        const result = await userCollection.insertOne(newUser);
+        res
+          .status(201)
+          .json({ message: "User registered successfully!", result: result });
+      } catch (error) {
+        res.status(500).json({ message: "Server error. Please try again." });
+        console.log(error);
+      }
+    });
+
+    app.post("/posts", async (req, res) => {
+      const newPost = req.body;
+
+      try {
+        const result = await postCollection.insertOne(newPost);
+        res
+          .status(201)
+          .json({ message: "Post created successfully!", result: result });
+      } catch (error) {
+        res.status(500).json({ message: "Server error. Please try again." });
+        console.log(error);
+      }
+    });
+
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('ChatterPoint API');
+app.get("/", (req, res) => {
+  res.send("ChatterPoint API");
 });
 
 app.listen(port, () => {
-    console.log(`ChatterPoint is running on port: http://localhost:${port}`);
-})
+  console.log(`ChatterPoint is running on port: http://localhost:${port}`);
+});
