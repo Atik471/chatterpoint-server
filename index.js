36f85872c7322b8a409a88d8550d9ba64237468a
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-01-16 21:32:08 +0600

    added fetch post details API

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8nuar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -66,16 +66,16 @@ async function run() {
     app.get("/posts", async (req, res) => {
       const { page = 1, limit = 5 } = req.query;
       const skip = (page - 1) * limit;
-    
+
       try {
         const posts = await postCollection
           .find({})
           .skip(skip)
           .limit(Number(limit))
           .toArray();
-    
+
         const totalPosts = await postCollection.countDocuments();
-    
+
         res.status(200).json({
           message: "Posts fetched successfully!",
           posts,
@@ -83,6 +83,26 @@ async function run() {
           totalPages: Math.ceil(totalPosts / limit),
           currentPage: page,
         });
+      } catch (error) {
+        res.status(500).json({ message: "Server error. Please try again." });
+        console.log(error);
+      }
+    });
+
+    app.get("/post/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid Post ID format" });
+      }
+
+      try {
+        const objectId = new ObjectId(id);
+        const post = await postCollection.findOne({ _id: objectId });
+        if (!post) {
+          return res.status(404).json({ message: "Post not found" });
+        }
+        res.status(200).json(post);
       } catch (error) {
         res.status(500).json({ message: "Server error. Please try again." });
         console.log(error);
